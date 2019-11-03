7375939263feb86c28b9f57a60aba359fc531063
Rangga Rifqi Pratama <rangga.rifqi.pratama@gmail.com>
2019-11-03 16:59:54 +0700

    Implement Searchbar component

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import SearchBar from './SearchBar';
 
 class App extends React.Component {
+
+  onSearchSubmit = (term) => {
+    console.log(term);
+  }
+
   render() {
-    return <div>Hello!</div>;
+    return(
+      <div className="ui container">
+        <SearchBar onSubmit={this.onSearchSubmit} />
+      </div>
+    );
   }
 }
 
